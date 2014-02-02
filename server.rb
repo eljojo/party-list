3e12a310f4c9a4cc70f80e2b585653906dd33525
Magdalena <madziaf86@tlen.pl>
2014-02-02 18:26:10 +0100

    created added.erb file

--- server.rb
+++ server.rb
@@ -4,12 +4,15 @@ set :bind, '0.0.0.0'
 people = []
 
 get '/person/:name' do
-  name_of_the_person = params[:name]
-  people << name_of_the_person
-  "ok #{name_of_the_person}, you're added to the list!"
+  @name_of_the_person = params[:name]
+  people << @name_of_the_person
+  erb :added
 end
+
 
 get '/' do
   @people = people
   erb :index
 end
+
+
