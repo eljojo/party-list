77be4f5dbd791a6fc200b3c6bd3f032e2d042dd2
Brigitte Markmann <bri.ma@gmx.de>
2014-02-02 18:07:59 +0100

    Showing Name of Person added to the list

--- server.rb
+++ server.rb
@@ -4,8 +4,9 @@ set :bind, '0.0.0.0'
 people = []
 
 get '/person/:name' do
-  people << params[:name]
-  "ok, you're added to the list!"
+  name_of_the_person = params[:name]
+  people << name_of_the_person
+  "ok #{name_of_the_person}, you're added to the list!"
 end
 
 get '/' do
